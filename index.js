46d006f98eed9ce93c37b82b9ebd99b64d941683
abujakariacse <abujakaria316@gmail.com>
2022-05-25 13:49:39 +0600

    all cart part find

--- index.js
+++ index.js
@@ -10,20 +10,6 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 const jwt = require('jsonwebtoken');
 
 
-const verifyJwt = (req, res, next) => {
-    const authHeader = req.headers.authorization
-    if (!authHeader) {
-        return res.status(401).send({ message: "Unauthorized Access" })
-    }
-    const token = authHeader.split(' ')[1];
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
-        if (err) {
-            return res.status(403).send({ message: "Forbidden Access" })
-        }
-        req.decoded = decoded
-        next()
-    })
-}
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zpz1r.mongodb.net/?retryWrites=true&w=majority`;
